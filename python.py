859b2936a19dda721e71daf9b60a40ebc5582297
PRINCE SAMLA <prince@Pros-MacBook-Pro.local>
2019-08-16 11:39:42 +0300

    datacleaning vl

---
+++ python.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import os
+import sys
+
+def myfunction(all):
+    print("Welcome\n")
+    print("EURON BANK ENTERPRISES\n")
+
+    password = '1234'
+
+    input(password)
+
+    if password != '1234':
+        print("Wrong password")
+
+    else:
+        print("Euron BANK\n")
+
+
+if __name__ == "__main__":
+
+    print("ENTER YOUR PASSWORD\t")
+
+    input(password)
+
+    print("\n")
+
+    myfunction(all)
+    
